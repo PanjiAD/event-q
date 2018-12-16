1227ffc3c3f710c7c63264b1d642793e603c600a
Panji Awwaludi D <panjiawwaludi430@gmail.com>
2018-12-16 16:21:02 +0700

    edit akun fix

--- akun.php
+++ akun.php
@@ -63,36 +63,39 @@
                         $user = mysqli_fetch_assoc($result);
                         ?>
                         <div class="row">
-                            <div class="col-9"><div class="nama"><?php echo $user['nama']?> </div></div>
-                            <div class="col-3 mt-4">
-                                <button type="button" class="btn" data-toggle="modal" data-target="#editAkun" style="background-color:rgb(243, 49, 23); color:#fff; border: 2px solid rgb(243, 49, 23);"><i class="fas fa-pencil-alt mr-2"></i>Edit</button>
+                            <div class="col-10">
+                                <div class="nama" style="font-size:2.5em"><?php echo $user['nama']?> </div>
+                                <div class="tanggal mb-3" style="color:rgb(121, 121, 121); font-size:0.9em"> Bergabung pada <?php echo $user['create_date']?> </div>
+                            </div>
+                            <div class="col-2 mt-4">
+                                <button type="button" class="btn" data-toggle="modal" data-target="#editAkun" style="background-color:rgb(243, 49, 23); color:#fff; border: 2px solid rgb(243, 49, 23);"><i class="fas fa-pencil-alt mr-2"></i>Edit Akun</button>
                             </div>
                         </div>
-                    <div class="form-group row">
+                    <div class="form-group row isi">
                         <label class="col-md-5">Username</label>
                         <div class="col-md-6">
                             <div class="username">: <?php echo $user['username']?></div>       
                         </div>                     
                     </div>
-                    <div class="form-group row">
+                    <div class="form-group row isi">
                         <label class="col-md-5">Email</label>
                         <div class="col-md-6">       
                             <div class="email">: <?php echo $user['email']?> </div>
                         </div>                     
                     </div>
-                    <div class="form-group row">
+                    <div class="form-group row isi">
                         <label class="col-md-5">Saldo</label>
                         <div class="col-md-6">       
                             <div class="saldo">: Rp <?php echo $user['saldo']?> </div>
                         </div>                     
                     </div>
-                    <div class="form-group row">
+                    <div class="form-group row isi">
                         <label class="col-md-5">Jumlah event </label>
                         <div class="col-md-6">       
                             <div class="event">:
                             <?php
-                                $user = $user['id_users'];
-                                $event = "SELECT COUNT(id_users) AS events FROM events WHERE id_users = '$user'";
+                                $user1 = $user['id_users'];
+                                $event = "SELECT COUNT(id_users) AS events FROM events WHERE id_users = '$user1'";
                                 $total_event = mysqli_query($con, $event);
                                 $row = mysqli_fetch_assoc($total_event);
                                 $event = $row['events'];
@@ -101,12 +104,12 @@
                             </div>
                         </div>                     
                     </div>
-                    <div class="form-group row">
+                    <div class="form-group row isi">
                         <label class="col-md-5">Jumlah Tiket </label>
                         <div class="col-md-6">       
                             <div class="tiket">: 
                             <?php 
-                                $tiket = "SELECT COUNT(id_users) AS tikets FROM tiket WHERE id_users = '$user'";
+                                $tiket = "SELECT COUNT(id_users) AS tikets FROM tiket WHERE id_users = '$user1'";
                                 $total_tiket = mysqli_query($con, $tiket);
                                 $row1 = mysqli_fetch_assoc($total_tiket);
                                 $tiket = $row1['tikets'];
@@ -125,33 +128,65 @@
     </div>
 </div>
 
-
-
 <div class="modal fade" id="editAkun" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Edit Akun</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
+      <form action="proses/editAkun.php" method="POST" enctype="multipart/form-data">
       <div class="modal-body">
-        <form>
-          <div class="form-group">
-            <label for="recipient-name" class="col-form-label">Recipient:</label>
-            <input type="text" class="form-control" id="recipient-name">
-          </div>
-          <div class="form-group">
-            <label for="message-text" class="col-form-label">Message:</label>
-            <textarea class="form-control" id="message-text"></textarea>
-          </div>
-        </form>
-      </div>
+        <input type="hidden" name="idUsers" value="<?php echo $user["id_users"] ?>">
+            <div class="row">
+                <div class="col-4">
+                    <div class="form-group">
+                      <label for="recipient-name" class="col-form-label">Foto Profil:</label>
+                      <img src="gambar/profil/<?php echo $user['gambar_profile']?>"/>
+                      <input type="hidden" name="gambar" value="<?php echo($username["gambar_profile"]) ?>"></br>
+                       <input type="file" name="gambar">
+                    </div>
+                </div>
+                    <div class="col-8">
+                        <div class="form-group">
+                          <label for="recipient-name" class="col-form-label">Nama Lengkap:</label>
+                          <input type="text" name="nama" class="form-control" value="<?php echo $user["nama"] ?>" placeholder="Nama Lengkap" required>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="form-group">
+                                  <label for="recipient-name" class="col-form-label">Username:</label>
+                                  <input type="text" name="username" class="form-control" value="<?php echo $user["username"] ?>" placeholder="Username" required>
+                                </div>
+                                <div class="form-group">
+                                  <label for="recipient-name" class="col-form-label">Password:</label>
+                                  <input type="password" name="pass" class="form-control" placeholder="Password">
+                                </div>
+                            </div>
+                                <div class="col-6">
+                                <div class="form-group">
+                                  <label for="recipient-name" class="col-form-label">Email:</label>
+                                  <input type="email" name="email" class="form-control" value="<?php echo $user["email"] ?>" placeholder="Nama Lengkap" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="recipient-name" class="col-form-label">Confirm Password:</label>
+                                    <input type="password" name="confirm" class="form-control" placeholder="Confirm Password">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                          <label for="recipient-name" class="col-form-label">Saldo:</label>
+                          <input type="number" name="saldo" class="form-control" value="<?php echo $user["saldo"] ?>" placeholder="Saldo" required>
+                        </div>
+                    </div>
+            </div>
+       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Send message</button>
+            <input type="submit" name="submit" value="Kirim Perubahan" class="btn btn-success">
       </div>
+      </form>
     </div>
   </div>
 </div>
---
+++ proses/editAkun.php
@@ -0,0 +1,82 @@
+<?php
+
+include '../helper/koneksi.php';
+    
+// Get the form update value
+$idUsers = $_POST["idUsers"];
+$nama = $_POST["nama"];
+$username = $_POST["username"];
+$email = $_POST["email"];
+$pass = $_POST["pass"];
+$saldo = $_POST["saldo"];
+$confirm = $_POST["confirm"];
+$gambar = $_POST["gambar"];
+
+$code = $_FILES["gambar"]["error"];
+  if($code === 0){
+
+    $nama_folder = "gambar/profil";
+    $tmp = $_FILES["gambar"]["tmp_name"];
+    $nama_file = $_FILES["gambar"]["name"];
+
+    $path = "../$nama_folder/$nama_file";
+    $oldPath = "../$nama_folder/$gambar";
+    $upload_check = false;
+
+    $tipe_file = array("image/jpeg", "image/gif", "image/png");
+    if(!in_array($_FILES["gambar"]["type"], $tipe_file))
+    {
+    	echo "<script> alert('cek kembali ekstensi file anda (*.jpeg,*.jpg,*.png,*.gif,)'); window.location = '../akun.php';</script>";
+        $upload_check = true;
+    }
+
+    if(!$upload_check AND move_uploaded_file($tmp, $path)){
+    	$error = urldecode("Upload Sukses");
+    }
+    
+    else{
+    	echo "<script> alert('Upload file gambar gagal'); window.location = '../akun.php';</script>";
+    }
+  }
+
+// Update query command
+if ($pass != '' && $confirm != '') {
+    if ($pass == $confirm) {
+        if (isset($nama_file)) {
+            $query = "UPDATE users 
+                SET nama = '$nama', username = '$username', pass = '$pass',  email = '$email', gambar_profile = '$nama_file', saldo = '$saldo'
+                WHERE id_users = '$idUsers'";
+            }
+            else{
+              $query = "UPDATE users 
+                SET nama = '$nama', username = '$username', pass = '$pass',  email = '$email', saldo = '$saldo'
+                WHERE id_users = '$idUsers'";
+            }
+    }
+    else{
+        echo "<script> alert('Confirm password tidak sesuai'); window.location = '../akun.php?id=$idUsers';</script>";
+    }
+        
+}
+else{
+    if (isset($nama_file)) {
+        $query = "UPDATE users 
+            SET nama = '$nama', username = '$username',  email = '$email', gambar_profile = '$nama_file', saldo = '$saldo'
+            WHERE id_users = '$idUsers'";
+    }
+    else{
+      $query = "UPDATE users 
+        SET nama = '$nama', username = '$username',  email = '$email', saldo = '$saldo'
+        WHERE id_users = '$idUsers'";
+    }
+}
+
+if (mysqli_query($con, $query)) {
+    header("Location: ../akun.php?id=$idUsers");
+} else {
+    echo "<script> alert('Data tidak berhasil di update'); window.location = '../akun.php?id=$idUsers';</script>";
+}
+// close mysql connection
+mysqli_close($con); 
+
+?>
