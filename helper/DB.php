dcdda2784f9fcfa125acd6bcf926d8cb18f3fb02
Panji Awwaludi D <panjiawwaludi430@gmail.com>
2018-12-20 18:14:35 +0700

    tambah port

--- helper/DB.php
+++ helper/DB.php
@@ -1,5 +1,5 @@
 <?php
-    define("HOST", "sql12.freesqldatabase.com");
+    define("HOST", "sql12.freesqldatabase.com:3306");
     define("UNAME", "sql12270869");
     define("PASS", "76DL9wCfWI");
     define("DB", "sql12270869");    
